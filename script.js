01b42c6ba8629e059db8fb44bbc4e8b94ecd61a9
Marco Rehberg <marcorehberg@gmx.net>
2025-09-04 14:51:13 +0200

    Preisstufen step1

--- script.js
+++ script.js
@@ -9,17 +9,25 @@ const antwortenWrapper = document.getElementById("antwortenWrapper");
 
 let currentQuestion = 0;
 
-const checkAnswer = event => {
+const checkAnswer = (event) => {
   console.log(event.target);
   const filteredArray = quizQuestions[currentQuestion].answers.filter(
-    answer =>
+    (answer) =>
       event.target.textContent === answer.text && answer.correct === true
   );
+
+  let preisstufen = document.getElementsByClassName("preisstufen");
+
+  for (let i = 0; i < preisstufen.length; i++) {
+    preisstufen[i].style.background = "inherit";
+  }
+
+  preisstufen[14 - currentQuestion].style.background = "red";
 
   if (filteredArray.length === 0) {
     event.target.style.background = "red";
     console.log("no correct answer");
-    // alert("das Spiel ist beendet");
+    // alert("Sie haben Verloren...");
 
     return;
   }
@@ -36,11 +44,11 @@ const checkAnswer = event => {
 const startGame = () => {
   frage.textContent = quizQuestions[currentQuestion].question;
 
-  quizQuestions[currentQuestion].answers.forEach(answer => {
+  quizQuestions[currentQuestion].answers.forEach((answer) => {
     const antwort = document.createElement("div");
     antwort.textContent = answer.text;
     antwortenWrapper.appendChild(antwort);
-    antwort.addEventListener("click", event => {
+    antwort.addEventListener("click", (event) => {
       checkAnswer(event);
     });
   });
